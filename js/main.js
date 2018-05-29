84df9ccd9b6da722bc74c64c48408d349858b7b9
Kamaropoulos <kamaropoulos@outlook.com>
2018-05-29 17:40:03 +0300

    Split SW registration from main.js SW can now also be loaded from the restaurant page

--- js/main.js
+++ js/main.js
@@ -176,17 +176,3 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 }
-
-
-// Register Service Worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js', { scope: ".."}).then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
