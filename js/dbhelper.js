36b9b717fba9744ca583ffe5fce0f6a5c4888efe
Kamaropoulos <kamaropoulos@outlook.com>
2018-06-30 01:11:45 +0300

    fetchRestaurantById will now only fetch the required restaurant instead of all of them

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -40,17 +40,29 @@ class DBHelper {
    */
   static fetchRestaurantById(id, callback) {
     // fetch all restaurants with proper error handling.
-    DBHelper.fetchRestaurants((error, restaurants) => {
-      if (error) {
-        callback(error, null);
-      } else {
-        const restaurant = restaurants.find(r => r.id == id);
-        if (restaurant) { // Got the restaurant
-          callback(null, restaurant);
-        } else { // Restaurant does not exist in the database
-          callback('Restaurant does not exist', null);
-        }
-      }
+    // DBHelper.fetchRestaurants((error, restaurants) => {
+    //   if (error) {
+    //     callback(error, null);
+    //   } else {
+    //     const restaurant = restaurants.find(r => r.id == id);
+    //     if (restaurant) { // Got the restaurant
+    //       callback(null, restaurant);
+    //     } else { // Restaurant does not exist in the database
+    //       callback('Restaurant does not exist', null);
+    //     }
+    //   }
+    // });
+
+
+
+    fetch(DBHelper.DATABASE_URL + '/' + id).then(function(response) {
+      return response.json();
+    }).then(function(response){
+      DBHelper.restaurantsJSON = response;
+      callback(null, response);
+    })
+    .catch(e => {
+      callback(e, null);
     });
   }
 
