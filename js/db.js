2f4813768955cc31582b88dbf89d863d621d2a18
Kamaropoulos <kamaropoulos@outlook.com>
2018-07-01 14:56:55 +0300

    feat: Added IDB store for Reviews

--- js/db.js
+++ js/db.js
@@ -1,56 +1,110 @@
 const dbPromise = idb.open('restaurants-store', 1, upgradeDB => {
-    upgradeDB.createObjectStore('restaurants');
-  });
-  
+  upgradeDB.createObjectStore('restaurants');
+  upgradeDB.createObjectStore('restaurants');
+});
+
 const restaurantsDB = {
-    get(key) {
-      return dbPromise.then(db => {
-        return db.transaction('restaurants')
-          .objectStore('restaurants').get(key);
+  get(key) {
+    return dbPromise.then(db => {
+      return db.transaction('restaurants')
+        .objectStore('restaurants').get(key);
+    });
+  },
+  getAll() {
+    return dbPromise.then(db => {
+      return db.transaction('restaurants')
+        .objectStore('restaurants').getAll();
+    });
+  },
+  set(key, val) {
+    return dbPromise.then(db => {
+      const tx = db.transaction('restaurants', 'readwrite');
+      tx.objectStore('restaurants').put(val, key);
+      return tx.complete;
+    });
+  },
+  delete(key) {
+    return dbPromise.then(db => {
+      const tx = db.transaction('restaurants', 'readwrite');
+      tx.objectStore('restaurants').delete(key);
+      return tx.complete;
+    });
+  },
+  clear() {
+    return dbPromise.then(db => {
+      const tx = db.transaction('restaurants', 'readwrite');
+      tx.objectStore('restaurants').clear();
+      return tx.complete;
+    });
+  },
+  keys() {
+    return dbPromise.then(db => {
+      const tx = db.transaction('restaurants');
+      const keys = [];
+      const store = tx.objectStore('restaurants');
+
+      // This would be store.getAllKeys(), but it isn't supported by Edge or Safari.
+      // openKeyCursor isn't supported by Safari, so we fall back
+      (store.iterateKeyCursor || store.iterateCursor).call(store, cursor => {
+        if (!cursor) return;
+        keys.push(cursor.key);
+        cursor.continue();
       });
-    },
-    getAll() {
-      return dbPromise.then(db => {
-        return db.transaction('restaurants')
-          .objectStore('restaurants').getAll();
+
+      return tx.complete.then(() => keys);
+    });
+  }
+};
+
+const reviewsDB = {
+  get(key) {
+    return dbPromise.then(db => {
+      return db.transaction('reviews')
+        .objectStore('reviews').get(key);
+    });
+  },
+  getAll() {
+    return dbPromise.then(db => {
+      return db.transaction('reviews')
+        .objectStore('reviews').getAll();
+    });
+  },
+  set(key, val) {
+    return dbPromise.then(db => {
+      const tx = db.transaction('reviews', 'readwrite');
+      tx.objectStore('reviews').put(val, key);
+      return tx.complete;
+    });
+  },
+  delete(key) {
+    return dbPromise.then(db => {
+      const tx = db.transaction('reviews', 'readwrite');
+      tx.objectStore('reviews').delete(key);
+      return tx.complete;
+    });
+  },
+  clear() {
+    return dbPromise.then(db => {
+      const tx = db.transaction('reviews', 'readwrite');
+      tx.objectStore('reviews').clear();
+      return tx.complete;
+    });
+  },
+  keys() {
+    return dbPromise.then(db => {
+      const tx = db.transaction('reviews');
+      const keys = [];
+      const store = tx.objectStore('reviews');
+
+      // This would be store.getAllKeys(), but it isn't supported by Edge or Safari.
+      // openKeyCursor isn't supported by Safari, so we fall back
+      (store.iterateKeyCursor || store.iterateCursor).call(store, cursor => {
+        if (!cursor) return;
+        keys.push(cursor.key);
+        cursor.continue();
       });
-    },
-    set(key, val) {
-      return dbPromise.then(db => {
-        const tx = db.transaction('restaurants', 'readwrite');
-        tx.objectStore('restaurants').put(val, key);
-        return tx.complete;
-      });
-    },
-    delete(key) {
-      return dbPromise.then(db => {
-        const tx = db.transaction('restaurants', 'readwrite');
-        tx.objectStore('restaurants').delete(key);
-        return tx.complete;
-      });
-    },
-    clear() {
-      return dbPromise.then(db => {
-        const tx = db.transaction('restaurants', 'readwrite');
-        tx.objectStore('restaurants').clear();
-        return tx.complete;
-      });
-    },
-    keys() {
-      return dbPromise.then(db => {
-        const tx = db.transaction('restaurants');
-        const keys = [];
-        const store = tx.objectStore('restaurants');
-  
-        // This would be store.getAllKeys(), but it isn't supported by Edge or Safari.
-        // openKeyCursor isn't supported by Safari, so we fall back
-        (store.iterateKeyCursor || store.iterateCursor).call(store, cursor => {
-          if (!cursor) return;
-          keys.push(cursor.key);
-          cursor.continue();
-        });
-  
-        return tx.complete.then(() => keys);
-      });
-    }
-  };
+
+      return tx.complete.then(() => keys);
+    });
+  }
+};
