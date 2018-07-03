f4d0475ae0912014876350a7a3822707213498ea
Kamaropoulos <kamaropoulos@outlook.com>
2018-07-04 00:25:22 +0300

    fix: Errors in JS files caused by minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,57 @@
 var gulp = require('gulp');
 let cleanCSS = require('gulp-clean-css');
-let minifyjs = require('gulp-js-minify');
+// let minifyjs = require('gulp-js-minify');
+var htmlmin = require('gulp-htmlmin');
+var uglifyjs = require('uglify-es');
+var composer = require('gulp-uglify/composer');
+var pump = require('pump');
+const imagemin = require('gulp-imagemin');
 
-gulp.task('minify-css', () => {
+var minify = composer(uglifyjs, console);
+
+gulp.task('styles', () => {
     return gulp.src('src/css/*.css')
-      .pipe(cleanCSS({compatibility: 'ie8'}))
-      .pipe(gulp.dest('dist/css'));
-  });
-
-gulp.task('minify-js', function(){
-    gulp.src('src/js/*.js')
-      .pipe(minifyjs())
-      .pipe(gulp.dest('dist/js'));
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('dist/css'));
 });
+
+gulp.task('scripts', function() {
+    var options = {};
+    pump([
+        gulp.src('src/js/*.js'),
+        minify(options),
+        gulp.dest('dist/js')
+      ]
+    );
+});
+
+gulp.task('sw', function() {
+    var options = {};
+    pump([
+        gulp.src('src/sw.js'),
+        minify(options),
+        gulp.dest('dist/')
+      ]
+    );
+});
+
+gulp.task('html', function () {
+    return gulp.src('src/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', () =>
+    gulp.src('src/img/**')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'))
+);
+
+gulp.task('default', [
+                        'styles',
+                        'sw',
+                        'scripts',
+                        'html',
+                        'images'
+                    ]
+        );
