5fc48e63cc1a5cbe673101280d9fe53bd6ac601c
Kamaropoulos <kamaropoulos@outlook.com>
2018-07-04 02:32:00 +0300

    feat: gulp now also copies manifest.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 let cleanCSS = require('gulp-clean-css');
-// let minifyjs = require('gulp-js-minify');
 var htmlmin = require('gulp-htmlmin');
 var uglifyjs = require('uglify-es');
 var composer = require('gulp-uglify/composer');
@@ -15,43 +14,49 @@ gulp.task('styles', () => {
         .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('scripts', function() {
+gulp.task('scripts', () => {
     var options = {};
     pump([
         gulp.src('src/js/*.js'),
         minify(options),
         gulp.dest('dist/js')
-      ]
+    ]
     );
 });
 
-gulp.task('sw', function() {
+gulp.task('sw', () => {
     var options = {};
     pump([
         gulp.src('src/sw.js'),
         minify(options),
         gulp.dest('dist/')
-      ]
+    ]
     );
 });
 
-gulp.task('html', function () {
+gulp.task('html', () => {
     return gulp.src('src/*.html')
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('images', () =>
-    gulp.src('src/img/**')
+gulp.task('images', () => {
+    return gulp.src('src/img/**')
         .pipe(imagemin())
-        .pipe(gulp.dest('dist/img'))
-);
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('copy', () => {
+    return gulp.src('src/manifest.json')
+        .pipe(gulp.dest('dist'));
+})
 
 gulp.task('default', [
-                        'styles',
-                        'sw',
-                        'scripts',
-                        'html',
-                        'images'
-                    ]
-        );
+    'styles',
+    'sw',
+    'scripts',
+    'html',
+    'images',
+    'copy'
+]
+);
